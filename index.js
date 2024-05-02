3b15834bd0a310cd51169be8c481f8d3b29cac36
shazvan-iwa <95293625+shazvan-iwa@users.noreply.github.com>
2024-05-02 11:44:20 +0530

    update css

--- index.js
+++ index.js
@@ -99,8 +99,7 @@ app.get("/:version/:cat/:type/:so_id", async (req, res) => {
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <title>IWA Document</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-                        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                    <link href="./css.css" rel="stylesheet">
                     <style>
                         
                         p {
